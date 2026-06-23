10ce1529f65367474d5f12d5e849da18e5427557
Abdo <abdooomokli@gmail.com>
2026-06-23 13:25:48 +0100

    Add Helloname.c to greet user by name

---
+++ 2026.6/23rd/Cs50problemsheet1/Helloname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    while (1)
+    {
+    char name[50];
+    printf ("what's your name  ");
+    scanf ("%s" , name);
+    
+    printf ("hello %s \n" , name);
+        
+    }
+}
